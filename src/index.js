d2bf39c739392a78fc25df3fb65242dcd5033c20
DartSter <kil-all@rambler.ru>
2023-05-01 20:08:33 +0300

    feat: add keyboard function and instruction

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import textarea from './js/textarea';
 import keybord from './js/keybord';
+import instruction from './js/Instruction';
 
 document.body.innerHTML = '<div class=\'main-container\'></div>';
 const mainContainer = document.querySelector('.main-container');
 
 mainContainer.append(textarea);
 mainContainer.append(keybord);
+mainContainer.append(instruction);
---
+++ src/js/Instruction.js
@@ -0,0 +1,11 @@
+const instruction = document.createElement('div');
+
+instruction.innerHTML = `<ul class="list">
+<li class="list__item">Keyboard for Windows</li>
+<li class="list__item">Change lenguage — alt + ctrl</li>
+<li class="list__item">Mouse click on shift, hold and move the mouse — shift will be active</li>
+<li class="list__item">Mouse click with hold on alt (ctrl) and click on ctrl (alt) — switch language</li>
+<li class="list__item">Text can be selected and several characters can be deleted using del or backspace</li>
+</ul>`;
+
+export default instruction;
