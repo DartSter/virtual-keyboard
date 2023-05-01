795cd5373c496b1447d9ab4d8b59cf4f4b84d7fa
DartSter <kil-all@rambler.ru>
2023-05-01 20:50:37 +0300

    fix: instruction mistakes

--- src/js/Instruction.js
+++ src/js/Instruction.js
@@ -4,7 +4,7 @@ instruction.innerHTML = `<ul class="list">
 <li class="list__item">Keyboard for Windows</li>
 <li class="list__item">Change lenguage — alt + ctrl</li>
 <li class="list__item">Mouse click on shift, hold and move the mouse — shift will be active</li>
-<li class="list__item">Mouse click with hold on alt (ctrl) and click on ctrl (alt) — switch language</li>
+<li class="list__item">Mouse click with hold on alt and click on ctrl — switch language</li>
 <li class="list__item">Text can be selected and several characters can be deleted using del or backspace</li>
 </ul>`;
 
